6e6013d45e270e0679c53ea9268137ac84f3c6c5
Matheus Oliveira <62664418+MathSilms@users.noreply.github.com>
2022-06-22 23:29:15 -0300

    integrando componentes na sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,58 @@
-export function Siderbar(){
+import { gql, useQuery } from '@apollo/client';
+import { Lesson } from './Lesson';
+
+const GET_LESSSONS_QUERY = gql`
+query {
+  lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+    id
+    lessonType
+    availableAt
+    title
+    slug
+  }
+}`
+
+interface getLessonsQueryResponse {
+    lessons: {
+        id: string;
+        title: string;
+        slug: string;
+        availableAt: string;
+        lessonType: 'live' | 'class';
+
+    }[]
+}
+
+
+
+export function Siderbar() {
+
+    const { data } = useQuery<getLessonsQueryResponse>(GET_LESSSONS_QUERY);
+
     return (
-        <h1>Siderbar</h1>
+        <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
+            <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
+                Cronograma de aulas
+            </span>
+
+            <div className='flex flex-col gap-8'>
+                {data?.lessons.map(lesson => {
+                    return (
+                        <Lesson
+                            key={lesson.id}
+                            title={lesson.title}
+                            slug={lesson.slug}
+                            availableAt={new Date(lesson.availableAt)}
+                            type={lesson.lessonType}
+                        />
+                    )
+                })}
+                {/* <Lesson />
+                <Lesson />
+                <Lesson />
+                <Lesson />
+                <Lesson /> */}
+            </div>
+        </aside>
     )
 }
